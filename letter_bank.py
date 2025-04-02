723aa4d481f068c3be6c12cf295565b057d77f1e
caninecrew <samuelleerumbley@gmail.com>
2025-04-01 22:25:00 -0500

    Add LETTER_VALUES dictionary to LetterBank for scoring letters

--- letter_bank.py
+++ letter_bank.py
@@ -31,6 +31,12 @@ class LetterBank:
                                             '0': 2, # bank tiles
                                         }
     
+    LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4,
+    'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3,
+    'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8,
+    'y': 4, 'z': 10, '0': 0} # bank values
+    
     def __init__(self):
         self.letter_bank = self.LETTER_FREQUENCIES.copy() 
         self.letter_bank_total = sum(self.letter_bank.values()) # total number of tiles in the bank
