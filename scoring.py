18ea59b06dc50eab2650e9b07303127406780df1
caninecrew <samuelleerumbley@gmail.com>
2025-04-02 15:11:20 -0500

    Implement scoring functionality with methods for calculating word and turn scores, applying special tiles, and validating word positions

---
+++ scoring.py
@@ -0,0 +1,19 @@
+__init__(self, special_tiles)
+
+calculate_word_score(self, word, positions)
+
+calculate_turn_score(self, words)
+
+get_letter_score(self, letter)
+
+apply_special_tiles(self, word, positions)
+
+is_bingo(self, word)
+
+get_total_score(self, words)
+
+reset_score(self)
+
+load_letter_score(self, letter_score_file)
+
+validate_word_positions(self, word, positions)
